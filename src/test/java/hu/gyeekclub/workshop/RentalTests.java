801cceb3a407d18815637770440362c968be1b18
Dominik Földi <dominik.foldi@outlook.com>
2019-06-25 19:06:49 +0200

    Add tests for new release type rental amount calculation

--- src/test/java/hu/gyeekclub/workshop/RentalTests.java
+++ src/test/java/hu/gyeekclub/workshop/RentalTests.java
@@ -15,10 +15,10 @@ public class RentalTests {
         String result = customer.statement();
 
         double expectedAmount = 1.5;
-        String expected = "Amount owed is " + expectedAmount;        
+        String expected = "Amount owed is " + expectedAmount;
         assertTrue(result.contains(expected));
     }
-    
+
     @Test
     public void testLongTermChildrenRentalAmountCalculation() {
         Movie movie = new Movie("Winnie the Pooh", Movie.CHILDRENS);
@@ -28,8 +28,47 @@ public class RentalTests {
         String result = customer.statement();
 
         double expectedAmount = 6.0;
-        String expected = "Amount owed is " + expectedAmount;        
+        String expected = "Amount owed is " + expectedAmount;
         assertTrue(result.contains(expected));
     }
 
+    @Test
+    public void testOneDayNewReleaseRentalAmountCalculation() {
+        Movie movie1 = new Movie("John Wick: Chapter 3 - Parabellum", Movie.NEW_RELEASE);
+        Rental rental1 = new Rental(movie1, 1);
+        Customer customer1 = new Customer("John Doe");
+        customer1.addRental(rental1);
+
+        String customer1Statement = customer1.statement();
+
+        assertEquals(customer1Statement,
+                "Rental Record for John Doe\n\tJohn Wick: Chapter 3 - Parabellum\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points");
+    }
+
+    @Test
+    public void testTwoDaysNewReleaseRentalAmountCalculation() {
+        Movie movie1 = new Movie("John Wick: Chapter 3 - Parabellum", Movie.NEW_RELEASE);
+        Rental rental1 = new Rental(movie1, 2);
+        Customer customer1 = new Customer("John Doe");
+        customer1.addRental(rental1);
+
+        String customer1Statement = customer1.statement();
+
+        assertEquals(customer1Statement,
+                "Rental Record for John Doe\n\tJohn Wick: Chapter 3 - Parabellum\t6.0\nAmount owed is 6.0\nYou earned 2 frequent renter points");
+    }
+
+    @Test
+    public void testMoreThanTwoDaysNewReleaseRentalAmountCalculation() {
+        Movie movie1 = new Movie("John Wick: Chapter 3 - Parabellum", Movie.NEW_RELEASE);
+        Rental rental1 = new Rental(movie1, 6);
+        Customer customer1 = new Customer("John Doe");
+        customer1.addRental(rental1);
+
+        String customer1Statement = customer1.statement();
+
+        assertEquals(
+            customer1Statement,
+            "Rental Record for John Doe\n\tJohn Wick: Chapter 3 - Parabellum\t18.0\nAmount owed is 18.0\nYou earned 2 frequent renter points");
+    }
 }
