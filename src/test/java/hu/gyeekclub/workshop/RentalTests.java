d4f628ed99d35b430785cc150bb35c98847f7edb
István Kádár <istvan.kadar@wyze.me>
2019-06-25 18:07:53 +0200

    An example test is added.

---
+++ src/test/java/hu/gyeekclub/workshop/RentalTests.java
@@ -0,0 +1,13 @@
+package hu.gyeekclub.workshop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RentalTests {
+
+    // example test
+    @Test
+    public void test() {
+        assertEquals(3,3);
+    }
+}
