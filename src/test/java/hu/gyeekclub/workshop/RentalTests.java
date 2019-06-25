5e5621262b27012a07d250ef7577efe0ce4d4d8c
István Kádár <istvan.kadar@wyze.me>
2019-06-25 18:51:45 +0200

    Tests for children-type rental amount calculation are added.

--- src/test/java/hu/gyeekclub/workshop/RentalTests.java
+++ src/test/java/hu/gyeekclub/workshop/RentalTests.java
@@ -2,12 +2,34 @@ package hu.gyeekclub.workshop;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class RentalTests {
 
-    // example test
     @Test
-    public void test() {
-        assertEquals(3,3);
+    public void testChildrenRentalAmountCalculation() {
+        Movie movie = new Movie("Winnie the Pooh", Movie.CHILDRENS);
+        Rental rental = new Rental(movie, 3);
+        Customer customer = new Customer("Judy Jusper");
+        customer.addRental(rental);
+        String result = customer.statement();
+
+        double expectedAmount = 1.5;
+        String expected = "Amount owed is " + expectedAmount;        
+        assertTrue(result.contains(expected));
     }
+    
+    @Test
+    public void testLongTermChildrenRentalAmountCalculation() {
+        Movie movie = new Movie("Winnie the Pooh", Movie.CHILDRENS);
+        Rental rental = new Rental(movie, 6);
+        Customer customer = new Customer("Judy Jusper");
+        customer.addRental(rental);
+        String result = customer.statement();
+
+        double expectedAmount = 6.0;
+        String expected = "Amount owed is " + expectedAmount;        
+        assertTrue(result.contains(expected));
+    }
+
 }
