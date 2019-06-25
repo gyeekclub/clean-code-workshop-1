cbed02122746685f50b717c97c6c5f3d8d0632a0
Tari Balázs <pro.balazs.tari@gmail.com>
2019-06-25 21:42:11 +0200

    new tests added:   - SecurityTests.java   - testing for code misusage resistance

---
+++ src/test/java/hu/gyeekclub/workshop/SecurityTests.java
@@ -0,0 +1,57 @@
+package hu.gyeekclub.workshop;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.lang.reflect.Executable;
+import java.lang.reflect.Method;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+// these tests test for code security: how much code allows itself to be misused
+// As a wise man once said: thou shalt make thine code resilient to tardiness!
+public class SecurityTests {
+
+    @Test
+    public void nullConstructorTest() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                new Customer(null);
+                new Movie(null, 0);
+                new Rental(null, 0);
+            },
+            "Do not allow null parameters to stay unpunished."
+        );
+    }
+
+    @Test
+    public void nullParameterTest() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                new Customer("Test").addRental(null);
+            },
+            "No not allow null parameters to stay unpunished."
+        );
+    }
+
+    @Test
+    public void illegalParameterTest() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                Customer customer = new Customer("Test");
+                Movie invalidMovie = new Movie("Nope", 80); // 80 is invalid
+                invalidMovie.setPriceCode(88); // even more illegal
+                customer.addRental(new Rental(invalidMovie, 5));
+                customer.statement();
+            },
+            "No not allow invalid parameters to happen."
+        );
+    }
+}
