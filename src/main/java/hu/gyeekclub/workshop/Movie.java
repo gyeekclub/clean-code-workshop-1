843377790dfd4292d77fa490abeff28522a903c8
Wagner Zoltan <zoltwagner@gmail.com>
2019-06-26 19:02:11 +0200

    If movieType is invalid IllegalArgumentException is thrown

--- src/main/java/hu/gyeekclub/workshop/Movie.java
+++ src/main/java/hu/gyeekclub/workshop/Movie.java
@@ -2,27 +2,35 @@ package hu.gyeekclub.workshop;
 
 public class Movie {
 
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
 
-	private String title;
-	private int priceCode;
+    private String title;
+    private int priceCode;
 
-	public Movie(String title, int priceCode) {
-		this.title = title;
-		this.priceCode = priceCode;
-	}
+    public Movie(String title, int priceCode) {
+        checkPriceCode(priceCode);
+        this.title = title;
+        this.priceCode = priceCode;
+    }
 
-	public int getPriceCode() {
-		return priceCode;
-	}
+    public int getPriceCode() {
+        return priceCode;
+    }
 
-	public void setPriceCode(int arg) {
-		priceCode = arg;
-	}
+    public void setPriceCode(int arg) {
+        checkPriceCode(arg);
+        priceCode = arg;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
+
+    private void checkPriceCode(int priceCode) {
+        if (priceCode < 0 || priceCode > 2) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
