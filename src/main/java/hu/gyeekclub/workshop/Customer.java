a1885ed58acd3c9c2840cbdd0206c2c3cf6284e1
Kulcsár Zoltán <zoltangamer@gmail.com>
2019-06-26 19:50:26 +0200

    Moved statement to new  class and new methods

--- src/main/java/hu/gyeekclub/workshop/Customer.java
+++ src/main/java/hu/gyeekclub/workshop/Customer.java
@@ -15,6 +15,10 @@ public class Customer {
 		this.name = name;
 	}
 
+	public Vector<Rental> getRentals() {
+		return rentals;
+	}
+
 	public void addRental(Rental arg) {
 		if(arg == null){
 			throw new IllegalArgumentException();
@@ -27,27 +31,7 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Enumeration rentals = this.rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental) rentals.nextElement();
-			//determine amounts for each line
-			thisAmount += each.getMovie().getIncriesedPrice(each.getDaysRented());
-
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if (each.getMovie().isFrequentRent(each.getDaysRented())) frequentRenterPoints++;
-			//show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
-		}
-		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-		return result;
+		CustomerRentingStatement customerRentingStatement = new CustomerRentingStatement(this);
+		return customerRentingStatement.toString();
 	}
 }
