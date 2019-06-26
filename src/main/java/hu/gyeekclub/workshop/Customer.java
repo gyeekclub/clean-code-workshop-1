8de6e6569eba9d1b0643e873b593ad582cf0e922
Lékó Bertalan <berc@proton.dev>
2019-06-26 19:38:07 +0200

    Rename variables to more meaningful names.

--- src/main/java/hu/gyeekclub/workshop/Customer.java
+++ src/main/java/hu/gyeekclub/workshop/Customer.java
@@ -21,23 +21,23 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int totalFrequentPoints = 0;
+		double totalRentalAmount = 0;
+		int totalLoyaltyPoints = 0;
 		Enumeration rentals = this.rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
+		String rentalRecord = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
 			Rental rental = (Rental) rentals.nextElement();
 			double thisAmount = rental.getAmount();
 			//determine amounts for each line
-			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
-			totalFrequentPoints += rental.getFrequentPointsForRental();
+			rentalRecord += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+			totalRentalAmount += thisAmount;
+			totalLoyaltyPoints += rental.getFrequentPointsForRental();
 		}
 
 		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(totalFrequentPoints) + " frequent renter points";
-		return result;
+		rentalRecord += "Amount owed is " + String.valueOf(totalRentalAmount) + "\n";
+		rentalRecord += "You earned " + String.valueOf(totalLoyaltyPoints) + " frequent renter points";
+		return rentalRecord;
 	}
 
 
