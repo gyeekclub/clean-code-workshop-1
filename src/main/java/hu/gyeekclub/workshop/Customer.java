e0cf0f2e0b92e4ebdc8cc92bc3014d80dc21b8da
Wagner Zoltan <zoltwagner@gmail.com>
2019-06-26 19:47:44 +0200

    extract method

--- src/main/java/hu/gyeekclub/workshop/Customer.java
+++ src/main/java/hu/gyeekclub/workshop/Customer.java
@@ -36,26 +36,11 @@ public class Customer {
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
+
+            double thisAmount = each.getAmountByTypeAndDay();
+
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
