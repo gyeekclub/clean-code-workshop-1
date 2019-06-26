7d54c595276e96c082a49cd5f29ae9e5c21dd5c3
Lékó Bertalan <berc@proton.dev>
2019-06-26 19:28:31 +0200

    Moved amount and frequentPoints to Rental object.

--- src/main/java/hu/gyeekclub/workshop/Rental.java
+++ src/main/java/hu/gyeekclub/workshop/Rental.java
@@ -17,4 +17,35 @@ public class Rental {
 	public Movie getMovie() {
 		return movie;
 	}
+
+	public double getAmount() {
+		double thisAmount = 0;
+		switch (this.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (this.getDaysRented() > 2)
+					thisAmount += (this.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += this.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (this.getDaysRented() > 3)
+					thisAmount += (this.getDaysRented() - 3) * 1.5;
+				break;
+		}
+	return thisAmount;
+	}
+
+	public int getFrequentPointsForRental() {
+		int frequentRenterPoints = 0;
+		// add frequent renter points
+		frequentRenterPoints++;
+		// add bonus for a two day new release rental
+		if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) frequentRenterPoints++;
+		//show figures for this rental
+		return frequentRenterPoints;
+	}
+
 }
